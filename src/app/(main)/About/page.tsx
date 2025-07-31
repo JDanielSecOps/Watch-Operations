b591476176915ea99bfda2ddce74812f2292a871
JDanielSecOps <josephdanielcharis@gmail.com>
2025-08-01 01:45:00 +0530

    changed icons

--- src/app/(main)/About/page.tsx
+++ src/app/(main)/About/page.tsx
@@ -45,7 +45,7 @@ const Index =()=>{
             href="/Accounts"
             para="A very simple implementation of authentication is performed
             to allow the user to visit the site and view the data using supabase"></CardComponentForIndex>
-            <CardComponentForIndex title="Alerts" svg={<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960"  fill="#FFFFFF"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>}  
+            <CardComponentForIndex title="Alerts" svg={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"  fill="#e3e3e3"><path d="m344-60-76-128-144-32 14-148-98-112 98-112-14-148 144-32 76-128 136 58 136-58 76 128 144 32-14 148 98 112-98 112 14 148-144 32-76 128-136-58-136 58Zm34-102 102-44 104 44 56-96 110-26-10-112 74-84-74-86 10-112-110-24-58-96-102 44-104-44-56 96-110 24 10 112-74 86 74 84-10 114 110 24 58 96Zm102-318Zm0 200q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Z"/></svg>}  
             alt="Alerts icon" 
             href="/Alerts"
             para="It is used to view the alerts which are raised in case of emergencies "></CardComponentForIndex>
